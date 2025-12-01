12d1c3559e3cd9024c0a5069ac22f2cdfd6f607a
alex <alexfer782@gmai.com>
2025-12-01 18:07:13 +0200

    lab3 inital

--- lab3.c
+++ lab3.c
@@ -1,33 +1,33 @@
-// #include "liste.c"
-// #include <stdio.h>
-// #include <stdlib.h>
-// #define CAPACITY 10
-//
-// void push(NodeT** stack, int key) {
-//     NodeT* newNode = (NodeT*)malloc(sizeof(NodeT));
-//     newNode->key = key;}
-//
-// NodeT* pop(NodeT** stack){
-//     if (*stack == NULL) {
-//         return NULL;
-//     }
-//     NodeT* topNode = *stack;
-//     *stack = (*stack)->next;
-//     return topNode;
-// }
-// typedef struct _queue
-// {
-// int vec[CAPACITY];
-// int size;
-// int head, tail;
-// }queueT;
-//
-// typedef struct node_type
-// {
-// KeyT key;
-// ValueT value;
-//
-// struct node_type *next;
-//
-// struct node_type *prev;
-// } NodeDL;
+#include "liste.c"
+#include <stdio.h>
+#include <stdlib.h>
+#define CAPACITY 10
+
+void push(NodeT** stack, int key) {
+    NodeT* newNode = (NodeT*)malloc(sizeof(NodeT));
+    newNode->key = key;}
+
+NodeT* pop(NodeT** stack){
+    if (*stack == NULL) {
+        return NULL;
+    }
+    NodeT* topNode = *stack;
+    *stack = (*stack)->next;
+    return topNode;
+}
+typedef struct _queue
+{
+int vec[CAPACITY];
+int size;
+int head, tail;
+}queueT;
+
+typedef struct node_type
+{
+int key;
+double value;
+
+struct node_type *next;
+
+struct node_type *prev;
+} NodeDL;
